68c700eef424a5fa61ade6a22fb978082f227109
Benjamin Herman <benjamin@metanomial.com>
2022-06-21 11:07:30 -0700

    Add friday entrypoint

--- dotw.js
+++ dotw.js
@@ -1,41 +1,30 @@
-const { Client } = require('discord.js')
-
-const CONSORTIUM = '537167820539166720'
-const GENERAL_CATEGORY = '609619276306972714'
-const DOTW_CATEGORY = '821808118807003136'
-const DOTW_CHANNELS = [
-  '821808450873851934', // Sunday
-  '821808212956676129', // Monday
-  '821808251061796874', // Tuesday
-  '821808287737577512', // Wednesday
-  '821808325209489408', // Thursday
-  '821808372684423179', // Friday
-  '821808406263496798' // Saturday
-]
+import {
+  CONSORTIUM,
+  DOTW_CATEGORY,
+  DOTW_CHANNELS,
+  GENERAL_CATEGORY,
+} from "./constants.js";
 
 /**
  * @param {Client} client
  */
-async function manageDaysOfTheWeek (client) {
-  const guild = client.guilds.resolve(CONSORTIUM)
-  const generalCategory = guild.channels.resolve(GENERAL_CATEGORY)
-  const dotwCategory = guild.channels.resolve(DOTW_CATEGORY)
-  const channels = DOTW_CHANNELS.map(id => guild.channels.resolve(id))
-  const weekday = new Date().getDay()
-  const currentDotw = channels[weekday]
+export async function manageDaysOfTheWeek(client) {
+  const a = 3;
+  const guild = client.guilds.resolve(CONSORTIUM);
+  const generalCategory = guild.channels.resolve(GENERAL_CATEGORY);
+  const dotwCategory = guild.channels.resolve(DOTW_CATEGORY);
+  const channels = DOTW_CHANNELS.map((id) => guild.channels.resolve(id));
+  const weekday = new Date().getDay();
+  const currentDotw = channels[weekday];
 
   // Move current DoTW channel to the top of the General category
-  await currentDotw.setParent(generalCategory)
-  await currentDotw.setPosition(0)
+  await currentDotw.setParent(generalCategory);
+  await currentDotw.setPosition(0);
 
   // Sort other DoTW channels in the DoTW category
   for (let i = 0; i < 6; ++i) {
-    const channel = channels[(weekday + i + 1) % 7]
-    await channel.setParent(dotwCategory)
-    await channel.setPosition(i)
+    const channel = channels[(weekday + i + 1) % 7];
+    await channel.setParent(dotwCategory);
+    await channel.setPosition(i);
   }
-}
-
-module.exports = {
-  manageDaysOfTheWeek
 }
--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-require('dotenv').config()
-const { Client, Intents } = require('discord.js')
-const { manageDaysOfTheWeek } = require('./dotw')
+import "dotenv/config";
+import { Client, Intents } from "discord.js";
+import { manageDaysOfTheWeek } from "./dotw.js";
+import { postFriday } from "./friday.js";
 
-const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] })
-const entrypoint = process.argv[2]
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const entrypoint = process.argv[2];
 
 if (!entrypoint) {
-  console.log('Must pass an entrypoint argument.')
-  process.exit(1)
+  console.log("Must pass an entrypoint argument.");
+  process.exit(1);
 }
 
 if (!process.env.DISCORD_TOKEN) {
-  console.log('DISCORD_TOKEN environment variable is not set.')
-  process.exit(1)
+  console.log("DISCORD_TOKEN environment variable is not set.");
+  process.exit(1);
 }
 
 // Login hook
-client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}.`)
-})
+client.once("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}.`);
+});
 
 switch (entrypoint) {
+  case "dotw": // Manage Days-of-the-week channels
+    client.login(process.env.DISCORD_TOKEN);
+    client.once(
+      "ready",
+      async () => {
+        await manageDaysOfTheWeek(client);
+        client.destroy();
+      },
+    );
+    break;
 
-  // Manage Days-of-the-week channels
-  case 'dotw':
-    client.login(process.env.DISCORD_TOKEN)
-    client.once('ready', async () => {
-      await manageDaysOfTheWeek(client)
-      client.destroy()
-    })
-    break
+  case "friday": // Post "Friday" by Rebecca Black
+    await postFriday();
+    break;
 
-  // Unknown entrypoint
-  default:
-    console.log(`Unknown entrypoint "${entrypoint}"`)
+  default: // Unknown entrypoint
+    console.log(`Unknown entrypoint "${entrypoint}"`);
 }
