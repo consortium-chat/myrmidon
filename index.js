92bb74b7fa64c4561c320b1a54bdae4de0b96c94
Benjamin Herman <benjamin@metanomial.com>
2022-06-21 10:02:55 -0700

    Login to discord from within entrypoints

--- index.js
+++ index.js
@@ -6,30 +6,32 @@ const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] })
 const entrypoint = process.argv[2]
 
 if (!entrypoint) {
-  console.log("Must pass an entrypoint argument.")
+  console.log('Must pass an entrypoint argument.')
   process.exit(1)
 }
 
 if (!process.env.DISCORD_TOKEN) {
-  console.log("DISCORD_TOKEN environment variable is not set.")
+  console.log('DISCORD_TOKEN environment variable is not set.')
   process.exit(1)
 }
 
 // Login hook
 client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}.`)
-  switch (entrypoint) {
-    
-    // Manage Days-of-the-week channels
-    case "dotw":
-      await manageDaysOfTheWeek(client)
-      break
-    
-    // Unknown entrypoint
-    default:
-      console.log(`Unknown entrypoint "${entrypoint}"`)
-  }
-  client.destroy()
 })
 
-client.login(process.env.DISCORD_TOKEN)
+switch (entrypoint) {
+
+  // Manage Days-of-the-week channels
+  case 'dotw':
+    client.login(process.env.DISCORD_TOKEN)
+    client.once('ready', async () => {
+      await manageDaysOfTheWeek(client)
+      client.destroy()
+    })
+    break
+
+  // Unknown entrypoint
+  default:
+    console.log(`Unknown entrypoint "${entrypoint}"`)
+}
