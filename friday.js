c155a1c0d84a5bf3ec2aa4c09ea34624c00ad517
Benjamin Herman <benjamin@metanomial.com>
2022-06-22 13:03:53 -0700

    Make friday independent script

--- friday.js
+++ friday.js
@@ -1,22 +1,22 @@
+import 'dotenv/config'
+import assert from 'node:assert/strict'
 import fetch from 'node-fetch'
-import { FRIDAY_WEBHOOK_ID, FRIDAY_WEBHOOK_TOKEN } from './constants.js'
 
-export async function postFriday () {
-  const endpoint = new URL(
-    `${FRIDAY_WEBHOOK_ID}/${FRIDAY_WEBHOOK_TOKEN}`,
-    'https://discord.com/api/webhooks/'
-  )
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      content: 'https://www.youtube.com/watch?v=kfVsfOSbJY0'
-    })
+assert(process.env.FRIDAY_WEBHOOK_URL, 'FRIDAY_WEBHOOK_URL environment variable is not set')
+
+fetch(process.env.FRIDAY_WEBHOOK_URL, {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json'
+  },
+  body: JSON.stringify({
+    content: 'https://www.youtube.com/watch?v=kfVsfOSbJY0'
   })
-  if (!response.ok) {
+}).then(async (response) => {
+  if (response.ok) {
+    console.log('Posted "Friday" by Rebecca Black')
+  } else {
     const data = await response.json()
     console.error('Post error:', data.message)
   }
-}
+}).catch(console.error)
