68c113fb44b9132f3b8a43734b04d04445333bb7
Benjamin Herman <benjamin@metanomial.com>
2023-01-22 01:24:27 -0800

    Add archive command
    - Migrate to the Pycord fork of Discord.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,18 +6,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from archive import ArchiveCommand
+
 token = os.environ["DISCORD_TOKEN"]
+archive_category_id = int(os.environ["ARCHIVE_CATEGORY_ID"])
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("myrmidon")
 
+
+class Myrmidon(discord.Bot):
+    async def on_ready(self):
+        logger.info(f"Logged in as {self.user}")
+
+
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-
-@client.event
-async def on_ready():
-    logger.info(f"Logged in as {client.user}")
-
-
-client.run(token)
+intents.messages = True
+bot = Myrmidon(intents=intents)
+bot.add_cog(ArchiveCommand(bot))
+bot.run(token)
---
+++ bot/archive.py
@@ -0,0 +1,47 @@
+import logging
+import os
+
+import discord
+
+archive_category_id = int(os.environ["ARCHIVE_CATEGORY_ID"])
+
+logger = logging.getLogger("myrmidon.archive")
+
+
+class ArchiveCommand(discord.Cog):
+    bot: discord.Bot
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.slash_command(
+        name="archive",
+        description="Move the current channel to the archive category",
+    )
+    async def archive_command(self, ctx: discord.ApplicationContext):
+
+        channel = ctx.channel
+        assert channel
+
+        if channel.type != discord.ChannelType.text:
+            await ctx.respond(
+                "This command can only be used in text channels", ephemeral=True
+            )
+            return
+
+        if channel.category and channel.category.id == archive_category_id:
+            await ctx.respond("This channel is already archived", ephemeral=True)
+            return
+
+        permissions = channel.permissions_for(ctx.author)
+
+        if not permissions.manage_channels:
+            await ctx.respond(
+                "You do not have permission to move this channel", ephemeral=True
+            )
+            return
+
+        archive_category = self.bot.get_channel(archive_category_id)
+        await channel.edit(category=archive_category)
+        await ctx.respond(f"Archived {channel.mention}.")
+        logger.info(f"Archived {channel.name} in {ctx.guild.name}")
